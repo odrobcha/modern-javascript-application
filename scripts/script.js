9624070d5f67596e7ad5581f80af82d68768f2e3
oksanka <drobchak.o@gmail.com>
2021-07-06 16:09:11 +0200

    code refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -29,49 +29,46 @@
         removeCardClass('error-info');
         document.getElementById('weather-icon').setAttribute('src', '');
     }
-    const clearErrorInfo = ()=>{
-        countriesElement.addEventListener('click', ()=>{
-            setInnerHTML('error-info', '');
-            removeCardClass('error-info');
-        });
-    };
+
     const createCanvasElement = () =>{
         let canvasElement = document.createElement('canvas');
         canvasElement.setAttribute('id', 'weatherChart');
         document.getElementById('weather-forecast').appendChild(canvasElement);
     };
 
+    const createSelectList = (selectItems, parentElement) =>{
+        let loopingArray = selectItems;
+        if (!Array.isArray(selectItems)) {
+            loopingArray = Object.keys(selectItems);
+        };
+        for (let item of loopingArray){
+            let optionElement = document.createElement('option');
+            optionElement.setAttribute('value', item);
+            optionElement.innerHTML = item;
+            parentElement.appendChild(optionElement);
+        };
+    };
+
     async function choseCity(){
         let data = await (fetch('json/country.json'));
         let countryList = await (data.json());
 
-        clearErrorInfo();
+        createSelectList (countryList, countriesElement);
 
-        for (let country in countryList){
-            let countryElement = document.createElement('option');
-            countryElement.setAttribute('value', country);
-            countryElement.innerHTML = country;
-            countriesElement.appendChild(countryElement);
-        };
 
-            countriesElement.addEventListener('click', ()=>{
-                let chosenCountry = countriesElement.value;
-                let chosenCitiesList =  countryList[chosenCountry];
+        countriesElement.addEventListener('click', ()=>{
 
-                if (chosenCitiesList === undefined){
-                    return
-                }
+            let chosenCountry = countriesElement.value;
+            let chosenCitiesList = countryList[chosenCountry];
+            if (chosenCitiesList === undefined){
+                return
+            }
+            document.getElementById('country-first-option').setAttribute('disabled', 'disabled');
+            citiesElement.innerHTML = '';
 
-                document.getElementById('country-first-option').setAttribute('disabled', 'disabled');
-                citiesElement.innerHTML = '';
+            createSelectList (chosenCitiesList, citiesElement);
 
-                for (let city of chosenCitiesList){
-                    let cityElement = document.createElement('option');
-                    cityElement.setAttribute('value', city);
-                    cityElement.innerHTML = city;
-                    citiesElement.appendChild(cityElement);
-                };
-            });
+        });
 
         showWeatherElement.addEventListener('click', ()=>{
             createCanvasElement();
@@ -79,22 +76,23 @@
             removeCardClass('error-info');
             removeCardClass('cities-info');
 
-            if (citiesElement.value == 'null'){
+            if (citiesElement.value === 'null'){
                 setInnerHTML('cities-info', 'Please, chose the city');
                 addCardClass('cities-info');
             } else {
                 cleanCurrentWeatherInfo();
                 document.getElementById('weatherChart').remove();
 
+                chosenCity = citiesElement.value;
+
+                getCurrentWeather(chosenCity);
+                getWeatherForecast(chosenCity);
+
                 addCardClass('weather-forecast');
                 addCardClass('today-weather');
                 setInnerHTML('cities-info', '');
                 setInnerHTML('today-weather-title', 'Now');
                 setInnerHTML('weather-forecast-title', 'Weather forecast');
-
-                chosenCity = citiesElement.value;
-                getCurrentWeather(chosenCity);
-                getWeatherForecast(chosenCity);
             };
      });
     };
@@ -102,8 +100,8 @@
     async function getCurrentWeather(city) {
         try {
             const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
-            const weather = await response.data.main;
 
+            const weather = await response.data.main;
             const weatherDescription = await response.data.weather[0];
             const weatherIconSrc = await `https://openweathermap.org/img/wn/${weatherDescription.icon}@2x.png`;
             const windSpeed = await response.data.wind.speed;
@@ -117,7 +115,6 @@
             setInnerHTML('temp-min', `Min: ${(weather.temp_min).toFixed(1)}&#8451`);
             setInnerHTML('humidity', `Humidity: ${(weather.humidity).toFixed(1)}%`);
             setInnerHTML('wind', `Wind: ${(windSpeed).toFixed(1)}km/h`);
-
 
         } catch (error) {
             cleanCurrentWeatherInfo();
@@ -143,8 +140,8 @@
                 if (item.dt_txt.includes("15:00:00")){
                     let day = item.dt_txt.substring(item.dt_txt.indexOf('-')+1 ,item.dt_txt.indexOf(' ')).split('-');
                     let date = day[1] + '/' + day[0];
-                    weatherIcon.push(`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`);
                     daysLabels.push(date);
+                    weatherIcon.push(`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`);
                     maxTemp.push((item.main.temp_max).toFixed(1));
                     weatherDescription.push(item.weather[0].description);
                 };
